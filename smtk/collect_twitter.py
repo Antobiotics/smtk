501f7ac4c2617f557847085da98ff0c6a55c85a9
Gregoire Lejay <gregoire.lejay@gmail.com>
2017-04-03 22:31:44 +0100

    :sparkles: get_friends command

--- smtk/collect_twitter.py
+++ smtk/collect_twitter.py
@@ -48,6 +48,9 @@ class CollectTwitter:
         if not screen_names is None:
             ids += self._screen_names_to_ids(screen_names)
 
+        print screen_names
+        print ids
+
         for id_ in ids:
             self._stream_friends_by_id(id_, request_limit=3)
 
--- smtk/commands/twitter/get_friends.py
+++ smtk/commands/twitter/get_friends.py
@@ -1,5 +1,27 @@
+import json
+
 import click
+
 from smtk.commands.cli import pass_context
+from smtk.collect_twitter import CollectTwitter
+
+class StdioTwitterLogger(CollectTwitter):
+    def on_tweet(self, tweet):
+        pass
+
+    def on_start(self):
+        pass
+
+    def on_profile(self, profile):
+        print profile
+
+    def on_connection(self, account, connection, type_):
+        edge = {
+            'account': account,
+            'connection': connection,
+            'type_': type_
+        }
+        print json.dumps(edge)
 
 
 @click.command('get_friends', short_help='Outputs Twitter friends for users in `--users`')
@@ -7,5 +29,6 @@ from smtk.commands.cli import pass_context
               help="CSV list of user_ids")
 @pass_context
 def cli(ctx, users):
-    print users
+    collector = StdioTwitterLogger()
+    collector.get_friends(screen_names=users.split(','))
 
